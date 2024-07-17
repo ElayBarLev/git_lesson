5670ab02a869ee3c2b97fc858279522e8257f1a9
Elay Barlev <elaybarlev@gmail.com>
2024-07-17 14:58:06 +0300

    update person

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Welcome to por... gitHUB");
+        Person person = new Person("popo",13,12);
+        System.out.println(person);
     }
 }
--- Person.java
+++ Person.java
@@ -1,6 +1,19 @@
 public class Person {
-    int shitPerDay;
-    double pissPerDay;
-    String numOnArm;
+    private String numOnArm;
+    private int shitPerDay;
+    private double pissPerDay;
 
+    public Person(String numOnArm, double pissPerDay, int shitPerDay) {
+        this.shitPerDay = shitPerDay;
+        this.pissPerDay = pissPerDay;
+        this.numOnArm = numOnArm;
+    }
+
+    @java.lang.Override
+    public java.lang.String toString() {
+        return "Person" + numOnArm
+                " shits " + shitPerDay +
+                "and piss " + pissPerDay +
+                " Per Day" +'\'';
+    }
 }
